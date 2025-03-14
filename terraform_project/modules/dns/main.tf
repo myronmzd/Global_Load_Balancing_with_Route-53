d9350dac0b2d06686c1532ac3f17032cdd7f817d
Myron <myronmzd22@gmail.com>
2025-03-14 04:31:36 +0000

    add last

--- terraform_project/modules/dns/main.tf
+++ terraform_project/modules/dns/main.tf
@@ -6,12 +6,26 @@ terraform {
     }
   }
 }
+variable "hosted_zone_id" {
+  type        = string
+  description = "The Route 53 hosted zone ID"
+}
 
+variable "instance_public_ip1" {
+  type        = string
+  description = "Public IP address for the first instance"
+}
+
+variable "instance_public_ip2" {
+  type        = string
+  description = "Public IP address for the second instance"
+}
 
 resource "aws_route53_health_check" "region1" {
   ip_address        = var.instance_public_ip1
   port              = 80
   type              = "HTTP"
+  resource_path     = "/"  # Add path to check
   request_interval  = "30"
   failure_threshold = "3"
 
@@ -21,9 +35,10 @@ resource "aws_route53_health_check" "region1" {
 }
 
 resource "aws_route53_health_check" "region2" {
-  ip_address        = var.instance_public_ip1
+  ip_address        = var.instance_public_ip2  # Fixed: Use IP of second instance
   port              = 80
   type              = "HTTP"
+  resource_path     = "/"  # Add path to check
   request_interval  = "30"
   failure_threshold = "3"
 
@@ -31,9 +46,9 @@ resource "aws_route53_health_check" "region2" {
     Name = "region2-health-check"
   }
 }
-# Latency-based routing records
+
 resource "aws_route53_record" "region1" {
-  zone_id = var.hosted_zone_id # Fixed: Use the zone_id from the primary zone
+  zone_id = var.hosted_zone_id
   name    = "www.myronmzd.com"
   type    = "A"
   ttl     = "300"
@@ -48,7 +63,7 @@ resource "aws_route53_record" "region1" {
 }
 
 resource "aws_route53_record" "region2" {
-  zone_id = var.hosted_zone_id # Fixed: Use the zone_id from the primary zone
+  zone_id = var.hosted_zone_id
   name    = "www.myronmzd.com"
   type    = "A"
   ttl     = "300"
@@ -58,6 +73,6 @@ resource "aws_route53_record" "region2" {
   }
 
   set_identifier  = "region2"
-  records         = [var.instance_public_ip1]
+  records         = [var.instance_public_ip2]  # Fixed: Use IP of second instance
   health_check_id = aws_route53_health_check.region2.id
 }
